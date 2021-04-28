ffbbec22b746f4ac6ae6944665fae21dd66f5011
sunghach <80687301+sunghach@users.noreply.github.com>
2021-04-28 22:43:15 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const spyEls = document.querySelectorAll('.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl,
+            triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
+});
